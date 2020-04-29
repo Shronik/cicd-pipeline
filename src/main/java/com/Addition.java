b77f51c41adfb7a45b4d6371cc31b9c11e333f3e
Shronik Thakur <shronikthakur@gmail.com>
2020-04-29 10:19:55 +0530

    Third commit

---
+++ src/main/java/com/Addition.java
@@ -0,0 +1,31 @@
+package com;
+
+public class Addition {
+	
+	public int add(int a, int b)
+	{
+		return a+b;
+	}
+	
+	public int substract(int a, int b)
+	{
+		return a-b;
+	}
+	
+	public int division(int a, int b)
+	{
+		return a/b;
+	}
+	public int multiply(int a, int b)
+	{
+		return a*b;
+	}
+	public String concat(String a, String b)
+	{
+		String c= "";
+		c=c+a;
+		c=c+b;
+		return c;
+	}
+	
+}
